f39b46c493be91b6a16b1ea571096bff30357142
Dontorn <dontorn@gmmail.com>
2021-10-25 09:00:16 +0200

    SingleServer commit

--- src/main/java/org/example/WorkClass.java
+++ src/main/java/org/example/WorkClass.java
@@ -3,5 +3,6 @@ package org.example;
 public class WorkClass {
     public void getResult(){
         System.out.println("This is master branch");
+        System.out.println("But now, this is singleserver branch");
     }
 }
